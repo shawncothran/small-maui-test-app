e3fe70028aa077d83a63192c32b4acbf40ae8165
Shawn Cothran <shawn.cothran@ramseysolutions.com>
2024-03-25 10:30:20 -0500

    modify string values for clarity

--- obj/Debug/net8.0-ios/iossimulator-arm64/SmallApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-ios/iossimulator-arm64/SmallApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmallApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d4c3fc4d4012606968eda6a864d9f20472130a23")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmallApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmallApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
